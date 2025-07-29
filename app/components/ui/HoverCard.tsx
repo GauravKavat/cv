fbc51f36a385082521eb7256c358582f0405d531
Gaurav Kavat <gauravkavat@gmail.com>
2025-07-30 00:09:23 +0530

    -

--- app/components/ui/HoverCard.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react';
-
-const HoverCard = ({ children }: { children: ReactNode }) => (
-    <div className="group relative mb-12">
-        {children}
-    </div>
-);
-
-export default HoverCard;
